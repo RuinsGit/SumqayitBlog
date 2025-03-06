77b3b360d5adbada8cf83664e572a1bf82384ee5
RuinsGit <museyibli.ruhin@gmail.com>
2025-03-06 05:03:29 -0800

    ruins

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,10 @@ use App\Http\Controllers\Admin\ContactRequestController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    if (auth()->guard('admin')->check()) {
+            return redirect()->route('back.pages.index');
+        }
+        return redirect()->route('admin.login');
 });
 
 Route::prefix('admin')->group(function () {
